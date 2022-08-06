475fb480dfcd1b403a83c15bc900d35a01b57d7f
Vinícius Martins <viniciusmartinsg@hotmail.com>
2022-08-06 09:07:27 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ import { environment } from './environment/environment';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
